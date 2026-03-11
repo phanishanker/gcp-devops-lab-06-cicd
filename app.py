8c5807d7ef2edfba0d902d524cec1e68790dbc08
Phanishanker Balle <phanishanker@gmail.com>
2026-03-11 18:18:24 +0000

    Initial Lab 6 CI/CD app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    version = os.getenv("APP_VERSION", "v1")
+    return f"Hello from Lab 6 CI/CD! Host={socket.gethostname()} Version={version}\n"
+
+@app.route("/health")
+def health():
+    return "OK\n", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
